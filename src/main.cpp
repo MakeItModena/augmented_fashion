d5355af3e682a688fc902cd37c090c236ba22b80
Dave Calaway <dave@Daves-MacBook-Air-2.local>
2019-06-19 22:12:45 +0200

    Stable

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,31 @@
- /*******************************************************************
+/*******************************************************************
     The sketch uses the Blynk library to control the colors.
 
     DaveCalaway & MakeITModena
  *******************************************************************/
+#define BLYNK_PRINT Serial
+
 #include <Arduino.h>
 #include <BlynkSimpleEsp8266.h>
 #include <ESP8266WiFi.h>
 #include <FastLED.h>
 
-// ----------- FastLed ---------------
-#define BLYNK_PRINT Serial  // Comment this out to disable prints and save space
-#define NUM_LEDS 5          // remeber, use an external power if u want to use leds>10
-#define DATA_PIN 5          // D5
+// --- FastLed ---
+#define NUM_LEDS 16  // remeber, use an external power (battery) if u want to use leds>10
+#define DATA_PIN D2  // D2
 CRGB leds[NUM_LEDS];
 #define BRIGHTNESS 128  // 0-255
-// -----------------------------------
-// if you want to use integer nuumber:  R*256*256 + G*256 + B = int
-int colors[NUM_LEDS * 3] = {249, 10, 10, 255, 102, 0, 250, 242, 14, 13, 255, 21, 13, 94, 255};  // only 2^8 colors
+
 bool power = 0;
-// ---------------------
+
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "abc";
+char auth[] = "xxxx";
 
 // Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "xxx";
-char pass[] = "yyy";
+char ssid[] = "makeitmodena";
+char pass[] = "password";
 
 // Add a on-off BUTTON Widget and set it to SWITH mode. Choose Virtual Pin V0
 BLYNK_WRITE(V0) {
@@ -44,19 +42,48 @@ BLYNK_WRITE(V0) {
 // Add a BUTTON Widget and set it to SWITH mode. Choose Virtual Pin V2
 BLYNK_WRITE(V2) {
     bool button = param.asInt();
-    int j = 0;
-    if (button == 1 && power == 1) {
-        Serial.println("colors");
-        for (int i = 0; i < NUM_LEDS; i++) {
-            leds[i].setRGB(colors[j], colors[j + 1], colors[j + 2]);
-            j = j + 3;
-        }
-        FastLED.show();
+    Serial.println("colors");
+
+    // turn off all leds
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CRGB::Black;
     }
+    FastLED.show();
+
+    if (button == 1 && power == 1) {
+        for (int flash = 0; flash < 100; flash++) {
+            FastLED.setBrightness(flash);
+            for (int i = 0; i < NUM_LEDS; i++) {
+                leds[i] = CRGB::Red;
+            }
+            FastLED.show();
+        }
+
+        for (int flash = 100; flash > 0; flash--) {
+            FastLED.setBrightness(flash);
+            for (int i = 0; i < NUM_LEDS; i++) {
+                leds[i] = CRGB::Red;
+            }
+            FastLED.show();
+        }
+    }
+
+    // turn off all leds
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CRGB::Black;
+    }
+    FastLED.show();
 }
 
 // Add a zeRGBa Widget and set it to MERGE mode. Choose Virtual Pin V1
 BLYNK_WRITE(V1) {
+    // turn off all leds
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CRGB::Black;
+    }
+    FastLED.show();
+
+    FastLED.setBrightness(BRIGHTNESS);
     if (power == 1) {
         int G = param[0].asInt();
         int R = param[1].asInt();
@@ -78,6 +105,7 @@ BLYNK_WRITE(V1) {
     }
 }
 
+// --- SETUP ---
 void setup() {
     Serial.begin(9600);
     FastLED.addLeds<WS2811, DATA_PIN>(leds, NUM_LEDS);
@@ -85,6 +113,7 @@ void setup() {
     Blynk.begin(auth, ssid, pass);
 }
 
+// --- LOOP ---
 void loop() {
     Blynk.run();
 }
